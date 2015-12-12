07881e117a2177816a56bcae99dde76cfd68e5c6
Aditya Pradhan <aditya.pradhan@chelsea-apps.com>
2015-12-12 01:17:13 +0000

    pull to refresh implemented

--- www/js/app.js
+++ www/js/app.js
@@ -49,6 +49,14 @@ angular.module('starter', ['ionic'])
     $scope.onItemDelete = function(item) {
     $scope.artists.splice($scope.artists.indexOf(item), 1)
     };
+
+    $scope.doRefresh = function() {
+      $http.get('js/data.json').success(function(data) {
+        $scope.artists = data;
+        $scope.$broadcast('scroll.refreshComplete');
+      });
+    }
+
     $scope.toggleStar = function(item) {
       item.star = !item.star;
     };
